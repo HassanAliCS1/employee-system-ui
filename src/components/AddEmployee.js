a0a1425f2daf510b6aa6a90aa1811903892e85bd
Hassan_ALi <hassan.ali@dailypay.com>
2023-11-22 14:47:04 +0000

    FE-004: made the endpoint work but bad code practice

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
+import axios from "axios";
 import EmployeeService from "../services/EmployeeService";
-import { configure } from "@testing-library/react";
 
 const AddEmployee = () => {
 
@@ -16,11 +16,16 @@ const AddEmployee = () => {
   }
   const saveEmployee = (e) => {
     e.preventDefault();
-    EmployeeService().saveEmployee(employee)
-    .then((response) => {console.log(response);})
-    .catch((error) => {console.log(error);
-    });
-  }
+    const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+    axios.post(EMPLOYEE_API_BASE_URL, employee);
+    // EmployeeService.saveEmployee(employee);
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  };
 
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b">
@@ -33,7 +38,7 @@ const AddEmployee = () => {
                     First Name
                 </label>
                 <input type="text" 
-                name = "first_name"
+                name="firstName"
                 value={employee.firstName}
                 onChange={(e)=> handleChange(e)}
                 className="h-10 w-96 border mt-2 px-2 py-2"></input>
@@ -54,7 +59,7 @@ const AddEmployee = () => {
                 </label>
                 <input type="text" 
                 name="emailId"
-                value={emailId}
+                value={employee.emailId}
                 onChange={(e)=> handleChange(e)}
                 className="h-10 w-96 border mt-2 px-2 py-2"></input>
             </div>
