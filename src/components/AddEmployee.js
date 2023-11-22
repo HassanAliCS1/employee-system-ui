0e16128bad2ed20b9b7fda666b685aa1e311af38
Hassan_ALi <hassan.ali@dailypay.com>
2023-11-22 10:49:53 +0000

    Added a service and connecting the inputs to the service which then hits the endpoint on BE

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,4 +1,27 @@
+import { useState } from "react"
+import EmployeeService from "../services/EmployeeService";
+import { configure } from "@testing-library/react";
+
 const AddEmployee = () => {
+
+  const [employee, setEmployee] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    emailId: ""
+  }) 
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setEmployee({...employee,[e.target.name]: value})
+  }
+  const saveEmployee = (e) => {
+    e.preventDefault();
+    EmployeeService().saveEmployee(employee)
+    .then((response) => {console.log(response);})
+    .catch((error) => {console.log(error);
+    });
+  }
+
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b">
        <div className="px-8 py-8">
@@ -9,22 +32,35 @@ const AddEmployee = () => {
                 <label className="block text-gray-600 text-sm font-normal">
                     First Name
                 </label>
-                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+                <input type="text" 
+                name = "first_name"
+                value={employee.firstName}
+                onChange={(e)=> handleChange(e)}
+                className="h-10 w-96 border mt-2 px-2 py-2"></input>
             </div>
             <div className="items-center justify-center h-14 w-full my-4">
                 <label className="block text-gray-600 text-sm font-normal">
                     Last Name
                 </label>
-                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+                <input type="text" 
+                name="lastName"
+                value={employee.lastName}
+                onChange={(e)=> handleChange(e)}
+                className="h-10 w-96 border mt-2 px-2 py-2"></input>
             </div>
             <div className="items-center justify-center h-14 w-full my-4">
                 <label className="block text-gray-600 text-sm font-normal">
                     Email
                 </label>
-                <input type="text" className="h-10 w-96 border mt-2 px-2 py-2"></input>
+                <input type="text" 
+                name="emailId"
+                value={emailId}
+                onChange={(e)=> handleChange(e)}
+                className="h-10 w-96 border mt-2 px-2 py-2"></input>
             </div>
-            <div className="items-center justify-center h-14 w-full my-4 space-x-4">
-                <button className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
+            <div className="items-center justify-center h-14 w-full my-4 space-x-4 ">
+                <button onClick={saveEmployee} 
+                className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
                     Save
                 </button>
                 <button className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">
